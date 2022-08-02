b447c89a3e691031b3c67cfe371d69826ed33ae7
Jon <>
2022-08-02 08:19:59 +0100

    Fixed warning about unused variable from array_pop

--- code/portal/stream.c
+++ code/portal/stream.c
@@ -18,7 +18,8 @@ void appendv(array_uint8_t *buffer, const char *fmt, va_list args)
     // We will pop this "\0" byte later.
     uint8_t *write_ptr = array_push(buffer, (size_t)ret + 1);
     vsnprintf((char *)write_ptr, (size_t)ret + 1, fmt, args_copy);
-    array_pop(buffer);
+    uint8_t removed = array_pop(buffer);
+    (void)removed;
     va_end(args_copy);
 }
 
