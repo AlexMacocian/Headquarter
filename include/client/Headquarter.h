7f27628090c5969100e97909e181f19f36e39701
Jon <jon.r+gh@live.com>
2023-12-21 17:19:19 +0000

    37314

--- include/client/Headquarter.h
+++ include/client/Headquarter.h
@@ -1,7 +1,7 @@
 #ifndef HEADQUARTER_H
 #define HEADQUARTER_H
 
-#define GUILD_WARS_VERSION          (37307)
+#define GUILD_WARS_VERSION          (37314)
 #define HEADQUARTER_VERSION_MAJOR   (1)
 #define HEADQUARTER_VERSION_MINOR   (0)
 
